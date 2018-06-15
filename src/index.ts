d6d49e1b31e31763b4d412dfc855803fc22d29e8
Simon Edwards <simon@simonzone.com>
2018-06-15 22:34:34 +0200

    Expose Anchor.

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,7 @@
  * Copyright (c) 2015-2018, David Holmes
  * Licensed under the 3-Clause BSD license. See the LICENSE file for details.
  */
+export { Anchor } from './Anchor';
 export { Delta } from './Delta';
 export { Document } from './Document';
 export { Editor } from './Editor';
